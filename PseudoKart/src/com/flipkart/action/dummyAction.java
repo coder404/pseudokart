2eebff73a50755dba88c334bc9a3d39c7a233130
varsha.raveendran <varsha.raveendran@gmail.com>
2013-03-20 04:49:48 +0000

    Initial commit

---
+++ PseudoKart/src/com/flipkart/action/dummyAction.java
@@ -0,0 +1,11 @@
+package com.flipkart.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class dummyAction extends ActionSupport{
+	
+	public String execute()
+	{
+		return "trial";
+	}
+}
