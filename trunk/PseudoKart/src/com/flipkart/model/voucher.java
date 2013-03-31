7e5a4f0cea2b1897a0674522908bde48da366c3b
yams25@gmail.com <yams25@gmail.com@8f66fd97-f6d5-878a-7723-5eb29028f415>
2013-03-31 13:14:20 +0000

    Voucher model class

--- trunk/PseudoKart/src/com/flipkart/model/voucher.java
+++ trunk/PseudoKart/src/com/flipkart/model/voucher.java
@@ -1,5 +1,75 @@
 package com.flipkart.model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.mast.util.DB;
+import com.mast.util.MyLog;
+
 public class voucher {
 
+	
+	int voucher_id;
+	String voucher_number;
+	String voucher_pin;
+	double balance;
+	
+	
+	public static ArrayList<voucher> findAll()
+	{
+		ArrayList<voucher> list=new ArrayList<voucher>();
+		ResultSet resultSet = null;
+		String query = "select * from voucher";
+		Connection connection = DB.getConnection();
+		resultSet = DB.readFromDB(query, connection);
+		try {
+			while (resultSet.next()) {
+				voucher v=new voucher();
+				v.voucher_number=resultSet.getInt("voucher_number")+"";
+				v.voucher_pin=resultSet.getInt("voucher_pin")+"";
+				v.balance=resultSet.getDouble("balance");
+			   list.add(v);
+			}
+		} catch (SQLException e) {
+	        MyLog.myCatch("Customer.java",50, e);
+			e.printStackTrace();
+		}
+		DB.close(resultSet);
+		DB.close(connection);
+		return list;
+	}
+	
+	public int getVoucher_id() {
+		return voucher_id;
+	}
+	public void setVoucher_id(int voucher_id) {
+		this.voucher_id = voucher_id;
+	}
+	
+	public String getVoucher_number() {
+		return voucher_number;
+	}
+
+	public void setVoucher_number(String voucher_number) {
+		this.voucher_number = voucher_number;
+	}
+
+	public String getVoucher_pin() {
+		return voucher_pin;
+	}
+
+	public void setVoucher_pin(String voucher_pin) {
+		this.voucher_pin = voucher_pin;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
 }
