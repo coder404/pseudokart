091a7a3cffaf37b9a16a8e60ce49f870ec046e92
laxmi21888 <laxmi21888@gmail.com>
2013-04-01 09:42:52 +0000

    with account Management Action classes

---
+++ PseudoKart/src/com/flipkart/action/accountManagementAction.java
@@ -0,0 +1,235 @@
+package com.flipkart.action;
+
+import java.util.Map;
+
+import com.flipkart.model.Customer;
+import com.flipkart.model.Login;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class accountManagementAction extends ActionSupport {
+	// User Profile variable declaration
+	private int id;
+	private String firstName = "";
+	private String lastName = "";
+	private String mobileNumber = "";
+	private String landlineNumber = "";
+	private String gender = "";
+	private String email = "";
+	private String profileName = "";
+	private String updatedProfileName = "";
+	Customer customerAction = new Customer();
+
+	Login login;
+
+	Map session;
+
+	// variables password
+	private String oldPassword;
+	private String newPassword;
+	private String reNewPassword;
+
+	public accountManagementAction() {
+		login = new Login();
+		login.setEmail("laxmi21888@gmail.com");
+		login.setPassword("lax123456");
+		session = ActionContext.getContext().getSession();
+		email = "laxmi21888@gmail.com";/*
+										 * (String)ActionContext.getContext().
+										 * getSession().get("email");
+										 */
+		session.put("email", email);
+	}
+
+	public String execute() {
+		/*
+		 * String
+		 * emailSession=(String)ActionContext.getContext().getSession().get
+		 * ("email");
+		 */
+		String mod = "where email='" + email + "'";
+		customerAction = Customer.findOne(mod);
+		return SUCCESS;
+	}
+
+	public String personalInformation() {
+		Customer cust = new Customer();
+		cust.setFirstName(firstName);
+		cust.setLastName(lastName);
+		cust.setMobileNumber(mobileNumber);
+		cust.setLandlineNumber(landlineNumber);
+		cust.setGender(gender);
+		cust.setEmail(email);
+		cust.updatePersonalInformation();
+
+		// String
+		// emailSession=(String)ActionContext.getContext().getSession().get("email");
+		String mod = "where email='" + email + "'";
+		customerAction = Customer.findOne(mod);
+		addActionError("Your Changes have been saved Successfully..");
+		return SUCCESS;
+	}
+
+	public String orderStatus() {
+
+		return SUCCESS;
+	}
+
+	public String changePasswordPage() {
+		return SUCCESS;
+	}
+
+	public String changePassword() {
+		// Find one object of login for the corresponding email
+		String mod = "where email='" + email + "'";
+		Login login = new Login();
+		login = Login.findOne(mod);
+		
+		if (oldPassword.isEmpty() || newPassword.isEmpty()
+				|| reNewPassword.isEmpty()) {
+			addActionError("Password Cannot be left empty.");
+			return "error";
+
+		} else if (!newPassword.equals(reNewPassword)) {
+			addActionError("Password change failed.New Passwords do not match.");
+			return "error";
+
+		} else if (!login.getPassword().equals(oldPassword)) {
+			addActionError("Email/Password combination is wrong.");
+			return "error";
+		} else {
+			Login login1=new Login();
+			login1.setEmail(email);
+			login1.setPassword(newPassword);
+			login1.update();
+			addActionMessage("Password Changed successfully.");
+			return SUCCESS;
+		}
+	}
+
+	// method just for address page
+	public String addresses() {
+
+		return SUCCESS;
+	}
+
+	public String updateEmail() {
+		return SUCCESS;
+	}
+
+	public String profileSettings() {
+		/*
+		 * String
+		 * emailSession=(String)ActionContext.getContext().getSession().get
+		 * ("email");
+		 */
+		String mod = "where email='" + email + "'";
+		System.out.println(mod);
+		customerAction = Customer.findOne(mod);
+		return SUCCESS;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getLandlineNumber() {
+		return landlineNumber;
+	}
+
+	public void setLandlineNumber(String landlineNumber) {
+		this.landlineNumber = landlineNumber;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getUpdatedProfileName() {
+		return updatedProfileName;
+	}
+
+	public void setUpdatedProfileName(String updatedProfileName) {
+		this.updatedProfileName = updatedProfileName;
+	}
+
+	public Customer getCustomerAction() {
+		return customerAction;
+	}
+
+	public void setCustomerAction(Customer customerAction) {
+		this.customerAction = customerAction;
+	}
+
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	public String getReNewPassword() {
+		return reNewPassword;
+	}
+
+	public void setReNewPassword(String reNewPassword) {
+		this.reNewPassword = reNewPassword;
+	}
+}
---
+++ PseudoKart/src/com/flipkart/action/addressAction.java
@@ -0,0 +1,162 @@
+package com.flipkart.action;
+
+import java.util.Map;
+
+import com.flipkart.model.Address;
+import com.flipkart.model.Customer;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class addressAction extends ActionSupport {
+	// variables declaration
+
+	private int address_id;
+	private String name = " ";
+	private String streetAddress = " ";
+	private String landmark = " ";
+	private String city = " ";
+	private String state = " ";
+	private String country = "India";
+	private String pin = " ";
+	private String phone = " ";
+	private int customer_id;
+	Map session;
+	Customer customerAction = new Customer();
+	 public boolean containsOnlyNumbers(String str) {
+	        
+	       for (int i = 0; i < str.length(); i++) {
+
+	            //If we find a non-digit character we return false.
+	            if (!Character.isDigit(str.charAt(i)))
+	                return false;
+	        }
+	        
+	        return true;
+	    }
+
+	public String execute() {
+		String email=(String)ActionContext.getContext().getSession().get("email");
+		String mod = "where email='" + email + "'";
+		customerAction = Customer.findOne(mod);
+		if(name.isEmpty() || streetAddress.isEmpty() || landmark.isEmpty() || city.isEmpty() || state.isEmpty() || pin.isEmpty() || phone.isEmpty()){
+			addActionError("Please provide all details.");
+			return "error";
+		}
+		else if(containsOnlyNumbers(phone) == false || pin.length() !=6){
+			addActionError("Please provide valid Pincode.");
+			return "error";
+	
+		}
+		else if(containsOnlyNumbers(phone) == false){
+			addActionError("Please provide valid Phone number.");
+			return "error";
+		}
+		else{
+			Address address=new Address();
+			//address.setAddress_id(address_id);
+			address.setName(name);
+			address.setStreetAddress(streetAddress);
+			address.setLandmark(landmark);
+			address.setCity(city);
+			address.setState(state);
+			address.setCountry(country);
+			address.setPin(pin);
+			address.setPhone(phone);
+			address.setCustomer_id(customerAction.getId());
+			System.out.println("Id"+ customerAction.getId());
+			
+			address.insert(customerAction.getId());
+			addActionMessage("Address saved successfully");
+			return SUCCESS;
+		}
+	}
+
+	public int getAddress_id() {
+		return address_id;
+	}
+
+	public void setAddress_id(int address_id) {
+		this.address_id = address_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getLandmark() {
+		return landmark;
+	}
+
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public Customer getCustomerAction() {
+		return customerAction;
+	}
+
+	public void setCustomerAction(Customer customerAction) {
+		this.customerAction = customerAction;
+	}
+
+}
