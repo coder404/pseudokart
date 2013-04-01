b701150a3d1b1c9700d07d53cc5cc8494a342580
varsha.raveendran@gmail.com <varsha.raveendran@gmail.com@8f66fd97-f6d5-878a-7723-5eb29028f415>
2013-04-01 22:42:29 +0000

    display the product selected

---
+++ trunk/PseudoKart/src/com/flipkart/action/displayProduct.java
@@ -0,0 +1,65 @@
+package com.flipkart.action;
+
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.flipkart.model.*;
+
+public class displayProduct extends ActionSupport {
+
+	
+	String productName;
+	float product_rating;
+	public float getProduct_rating() {
+		return product_rating;
+	}
+
+	public void setProduct_rating(float product_rating) {
+		this.product_rating = product_rating;
+	}
+
+	Product product = new Product();
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String execute() {
+
+		Map session = ActionContext.getContext().getSession();
+		String sql = "where name = '" + productName + "'";
+		System.out.println("sql=" + sql);
+		
+	product	 = Product.findProduct(sql);
+		
+	sql = "where productId = '" + product.getProdid() + "'";
+	System.out.println("SQL :" + sql);
+	 product_rating = Product.ratingsProduct(sql);
+	System.out.println(product_rating);
+	session.put("productID",product_rating);
+	
+				
+		
+		
+		return "success";
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+}
