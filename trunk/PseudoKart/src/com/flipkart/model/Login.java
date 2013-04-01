8f9ccac29a4fbd62b2c3ac04329a3c45ea6e6565
laxmi21888@gmail.com <laxmi21888@gmail.com@8f66fd97-f6d5-878a-7723-5eb29028f415>
2013-04-01 09:41:17 +0000

    login.java with update and findOne method

--- trunk/PseudoKart/src/com/flipkart/model/Login.java
+++ trunk/PseudoKart/src/com/flipkart/model/Login.java
@@ -1,5 +1,12 @@
 package com.flipkart.model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mast.util.DB;
+import com.mast.util.MyLog;
+
 public class Login {
 	private String email;
 	private String password;
@@ -15,5 +22,43 @@ public class Login {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public static Login findOne(String selectionModifier)
+	{
+
+		ResultSet rs=null;
+		String query = "select * from login " + selectionModifier;
+		//System.out.println(query);
+		Connection connection = DB.getConnection();
+		rs = DB.readFromDB(query, connection);
+		try {
+			if (rs.next()) {
+				Login login=new Login();
+				login.email=rs.getString("email");
+				login.password=rs.getString("password");
+				DB.close(rs);
+				DB.close(connection);
+				return login;
+			}
+			
+		} catch (SQLException e) {
+			MyLog.myCatch("Customer.java", 70, e);
+			e.printStackTrace();
+		}
+		DB.close(rs);
+		DB.close(connection);
+		return null;
+	}
 	
+	public int update() {
+		// TODO Auto-generated method stub
+		String updateSQL = 
+				"update login "
+						+ "set password= '" + password +"'" 
+						+" where email='"+email +"'";
+		//System.out.println("query = " + updateSQL);
+		return DB.update(updateSQL);
+	
+		
+	}
+
 }
