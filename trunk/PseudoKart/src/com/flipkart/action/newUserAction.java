7fe1e162eb7ba668c0bdaaf1b23a41bee4ba8cff
laxmi21888@gmail.com <laxmi21888@gmail.com@8f66fd97-f6d5-878a-7723-5eb29028f415>
2013-04-02 10:11:10 +0000

    calling customer.insert()

--- trunk/PseudoKart/src/com/flipkart/action/newUserAction.java
+++ trunk/PseudoKart/src/com/flipkart/action/newUserAction.java
@@ -1,5 +1,6 @@
 package com.flipkart.action;
 
+import com.flipkart.model.Customer;
 import com.flipkart.model.Login;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -8,6 +9,14 @@ public class newUserAction extends ActionSupport{
 	private String email;
 	private String password;
 	private String repeatPassword;
+	
+	private String firstName="";
+	private String lastName="";
+	private String mobileNumber="";
+	private String landlineNumber="";
+	private String gender="";
+	private String profileName="";
+	private String updatedProfileName="";
 	
 	public String getEmail() {
 		return email;
@@ -62,12 +71,84 @@ public class newUserAction extends ActionSupport{
 		login.setEmail(email);
 		login.setPassword(password);
 		flag = login.insert();
+		int minimum=400;
+		int maximum=2000;
+		String random,subemail;
+		random=minimum + (int)(Math.random()*maximum)+"";
+		subemail=email.substring(1,5);
+		profileName=subemail+random;
+		Customer newCustomer=new Customer();
+		newCustomer.setFirstName("");
+		newCustomer.setLastName("");
+		newCustomer.setGender("");
+		newCustomer.setMobileNumber("");
+		newCustomer.setLandlineNumber("");
+		newCustomer.setUpdatedProfileName("");
+		newCustomer.setEmail(email);
+		newCustomer.setProfileName(profileName);
 		
+		newCustomer.insert();
 		if(flag == 1)
 			return "success";
 		else {
 			//addActionMessage(ERROR);
 			return "error";
 		}
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getLandlineNumber() {
+		return landlineNumber;
+	}
+
+	public void setLandlineNumber(String landlineNumber) {
+		this.landlineNumber = landlineNumber;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getUpdatedProfileName() {
+		return updatedProfileName;
+	}
+
+	public void setUpdatedProfileName(String updatedProfileName) {
+		this.updatedProfileName = updatedProfileName;
 	}
 }
