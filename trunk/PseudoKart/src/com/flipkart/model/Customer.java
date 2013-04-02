17639d70a750d9b7addb910262777cf1773a5001
laxmi21888@gmail.com <laxmi21888@gmail.com@8f66fd97-f6d5-878a-7723-5eb29028f415>
2013-04-02 10:11:40 +0000

    insert method added

--- trunk/PseudoKart/src/com/flipkart/model/Customer.java
+++ trunk/PseudoKart/src/com/flipkart/model/Customer.java
@@ -8,97 +8,111 @@ import java.util.ArrayList;
 import com.mast.util.DB;
 import com.mast.util.MyLog;
 
-
 public class Customer {
 	private int id;
-	private String firstName="";
-	private String lastName="";
-	private String mobileNumber="";
-	private String landlineNumber="";
-	private String gender="";
-	private String email="";
-	private String profileName="";
-	private String updatedProfileName="";
-	
-	
+	private String firstName = "";
+	private String lastName = "";
+	private String mobileNumber = "";
+	private String landlineNumber = "";
+	private String gender = "";
+	private String email = "";
+	private String profileName = "";
+	private String updatedProfileName = "";
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
 	public String getLandlineNumber() {
 		return landlineNumber;
 	}
+
 	public void setLandlineNumber(String landlineNumber) {
 		this.landlineNumber = landlineNumber;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getProfileName() {
 		return profileName;
 	}
+
 	public void setProfileName(String profileName) {
 		this.profileName = profileName;
 	}
+
 	public String getUpdatedProfileName() {
 		return updatedProfileName;
 	}
+
 	public void setUpdatedProfileName(String updatedProfileName) {
 		this.updatedProfileName = updatedProfileName;
 	}
-	
-	
+
 	public static ArrayList<Customer> find(String selectionModifier) {
 		ArrayList<Customer> selection = new ArrayList<Customer>();
 		ResultSet resultSet = null;
-		String query = "select * " +
-				"from customer " + selectionModifier;
+		String query = "select * " + "from customer " + selectionModifier;
 		Connection connection = DB.getConnection();
 		resultSet = DB.readFromDB(query, connection);
 		try {
 			while (resultSet.next()) {
-				Customer customer=new Customer();
-				customer.id=resultSet.getInt("id");
-				customer.firstName=resultSet.getString("firstName");
-				customer.lastName=resultSet.getString("lastName");
-				customer.mobileNumber=resultSet.getString("mobileNumber");
-				customer.landlineNumber=resultSet.getString("landlineNumber");
-				customer.gender=resultSet.getString("gender");
-				customer.profileName=resultSet.getString("profileName");
-				customer.updatedProfileName=resultSet.getString("updatedProfileName");
+				Customer customer = new Customer();
+				customer.id = resultSet.getInt("id");
+				customer.firstName = resultSet.getString("firstName");
+				customer.lastName = resultSet.getString("lastName");
+				customer.mobileNumber = resultSet.getString("mobileNumber");
+				customer.landlineNumber = resultSet.getString("landlineNumber");
+				customer.gender = resultSet.getString("gender");
+				customer.profileName = resultSet.getString("profileName");
+				customer.updatedProfileName = resultSet
+						.getString("updatedProfileName");
 				selection.add(customer);
 			}
 		} catch (SQLException e) {
-	        MyLog.myCatch("Customer.java",50, e);
+			MyLog.myCatch("Customer.java", 50, e);
 			e.printStackTrace();
 		}
 		DB.close(resultSet);
@@ -106,61 +120,63 @@ public class Customer {
 		return selection;
 	}
 
+	public static Customer findOne(String selectionModifier) {
 
-
-		public static Customer findOne(String selectionModifier)
-		{
-
-			ResultSet rs=null;
-			String query = "select * from customer " + selectionModifier;
-			//System.out.println(query);
-			Connection connection = DB.getConnection();
-			rs = DB.readFromDB(query, connection);
-			try {
-				if (rs.next()) {
-					Customer customer=new Customer();
-					customer.id=Integer.parseInt(rs.getString("id"));
-					customer.firstName=rs.getString("firstName");
-					customer.lastName=rs.getString("lastName");
-					customer.mobileNumber=rs.getString("mobileNumber");
-					customer.landlineNumber=rs.getString("landlineNumber");
-					customer.gender=rs.getString("gender");
-					customer.profileName=rs.getString("profileName");
-					customer.updatedProfileName=rs.getString("updatedProfileName");
-					DB.close(rs);
-					DB.close(connection);
-					return customer;
-				}
-				
-			} catch (SQLException e) {
-				MyLog.myCatch("Customer.java", 70, e);
-				e.printStackTrace();
+		ResultSet rs = null;
+		String query = "select * from customer " + selectionModifier;
+		// System.out.println(query);
+		Connection connection = DB.getConnection();
+		rs = DB.readFromDB(query, connection);
+		try {
+			if (rs.next()) {
+				Customer customer = new Customer();
+				customer.id = Integer.parseInt(rs.getString("id"));
+				customer.firstName = rs.getString("firstName");
+				customer.lastName = rs.getString("lastName");
+				customer.mobileNumber = rs.getString("mobileNumber");
+				customer.landlineNumber = rs.getString("landlineNumber");
+				customer.gender = rs.getString("gender");
+				customer.profileName = rs.getString("profileName");
+				customer.updatedProfileName = rs
+						.getString("updatedProfileName");
+				DB.close(rs);
+				DB.close(connection);
+				return customer;
 			}
-			DB.close(rs);
-			DB.close(connection);
-			return null;
+
+		} catch (SQLException e) {
+			MyLog.myCatch("Customer.java", 70, e);
+			e.printStackTrace();
 		}
+		DB.close(rs);
+		DB.close(connection);
+		return null;
+	}
 
-		public int updatePersonalInformation() {
-			// TODO Auto-generated method stub
-			String updateSQL = 
-					"update customer "
-							+ "set firstName= '" + firstName + "',"
-							+ "lastName= '" + lastName + "',"
-							+ "mobileNumber= '" + mobileNumber + "',"
-							+ "landlineNumber= '" + landlineNumber + "',"
-							+" gender = '" + gender +"'" 
-							+" where email='"+email +"'";
-			System.out.println("query = " + updateSQL);
-			return DB.update(updateSQL);
-		
-			
-		}
+	public int updatePersonalInformation() {
+		// TODO Auto-generated method stub
+		String updateSQL = "update customer " + "set firstName= '" + firstName
+				+ "'," + "lastName= '" + lastName + "'," + "mobileNumber= '"
+				+ mobileNumber + "'," + "landlineNumber= '" + landlineNumber
+				+ "'," + " gender = '" + gender + "'" + " where email='"
+				+ email + "'";
+		System.out.println("query = " + updateSQL);
+		return DB.update(updateSQL);
 
+	}
 
+	public int insert() {
+		// TODO Auto-generated method stub
+		String insertSQL = "insert into address"
+				+ "(firstName,lastName,mobileNumber,landlineNumber,gender,email,profileName,updatedProfileName,CartAppendNo) "
+				+ "values(" + "'" + firstName + "','" + lastName + "', '"
+				+ mobileNumber + "','" + landlineNumber + "' , '" + gender + "' , '" + email
+				+ "','" + profileName + "','" + updatedProfileName + "', '" + 1 + "' );";
 
+		System.out.println(insertSQL);
 
+		return DB.update(insertSQL);
 
-	
-	
+	}
+
 }
