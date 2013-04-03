95383eda4913765c3fa2e4214674fc5b4a846180
preethi.ypr <preethi.ypr@gmail.com>
2013-04-03 17:11:56 +0000

    New action to redirect to login.jsp

---
+++ PseudoKart/src/com/flipkart/action/login_dummy.java
@@ -0,0 +1,14 @@
+package com.flipkart.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class login_dummy extends ActionSupport{
+	
+	
+	public String execute()
+	{
+		
+		return "success";
+	}
+
+}
