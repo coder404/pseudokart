0a8ba7cee8478feb8cb4d98a8088621ee965e5a2
varsha.raveendran <varsha.raveendran@gmail.com>
2013-04-10 07:48:45 +0000

    methods for update stock and notify me added

--- PseudoKart/src/com/flipkart/action/stockAction.java
+++ PseudoKart/src/com/flipkart/action/stockAction.java
@@ -1,7 +1,9 @@
 package com.flipkart.action;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.flipkart.model.Product;
 import com.flipkart.model.Stock;
@@ -27,13 +29,32 @@ public class stockAction extends ActionSupport{
 	String subject;
 	String msg;
 	String err_msg="";
+	private String stock_quantity;
+	ArrayList<Product> stklist;
+	
+	Map session = ActionContext.getContext().getSession();
 	
 	
-	
+	public ArrayList<Product> getStklist() {
+		return stklist;
+	}
+
+	public void setStklist(ArrayList<Product> stklist) {
+		this.stklist = stklist;
+	}
+
+	public String getStock_quantity() {
+		return stock_quantity;
+	}
+
+	public void setStock_quantity(String stock_quantity) {
+		this.stock_quantity = stock_quantity;
+	}
+
 	public String execute()
 	{
 		list=Product.findAll("");
-		
+		session.put("stock_list", list);
 		if(!err_msg.isEmpty())
 			addActionError(err_msg);
 		return SUCCESS;
@@ -132,6 +153,126 @@ public class stockAction extends ActionSupport{
 				return "empty";
 	}
 }
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public String update_stock(){
+		stklist = (ArrayList<Product>)session.get("stock_list");
+	for(int i=0 ; i<stklist.size() ; i++)
+		System.out.println(stklist.get(i).getProdid());
+		
+		System.out.println("QUANTITY " + stock_quantity);
+		
+		String[] qty = stock_quantity.split(",");
+		for (int i = 0; i<qty.length;i++)
+		{
+		//	System.out.println(Integer.parseInt(qty[i]));
+			try
+			{
+			int quantity = Integer.parseInt(qty[i].trim());
+			System.out.println(stklist.get(i).getProdid());
+			
+			String sql =  "where stockProductId='" + stklist.get(i).getProdid() + "';";
+			Stock s = Stock.findOne(sql);
+			
+			if(s == null)
+			{
+				System.out.println("Not there");
+				sql = "'" + stklist.get(i).getProdid() + "'," + quantity ;
+				Stock.insertStock(sql);
+				
+			}
+			
+			else{
+			sql =  quantity + " where stockProductId='" + stklist.get(i).getProdid() + "'";
+			Stock.updateStock(sql);
+			}
+			
+			notify_customer(stklist.get(i).getProdid());
+
+			}
+			catch(NumberFormatException nfe)
+			{
+				System.out.println("Blank");
+			}
+			/*if(qty[i].equals(" "))
+			{
+				System.out.println(stklist.get(i).getProdid());
+			}*/
+		}
+		
+		
+		return "success";
+	}
+	private void notify_customer(String prodid) {
+
+		String sql =  "where productId='" + prodid + "';";
+		ArrayList<String> emailids = Stock.findAllNotifyMe(sql);
+		for(int i=0 ; i < emailids.size() ; i++)
+		{
+			System.out.println(emailids.get(i));
+		}
+		
+		
+		for(int i =0 ; i < emailids.size();i++)
+		{
+		
+		 try
+		 {
+			
+			 String host = "smtp.gmail.com";
+			 String from = "com.iiitb.pseudokart";
+			 String pass = "kart@123";
+			 Properties props = System.getProperties();
+			 props.put("mail.smtp.starttls.enable", "true"); // added this line
+			 props.put("mail.smtp.host", host);
+			 props.put("mail.smtp.user", from);
+			 props.put("mail.smtp.password", pass);
+			 props.put("mail.smtp.port", "587");
+			 props.put("mail.smtp.auth", "true");
+
+			 String to = emailids.get(i); // added this line
+			 System.out.println("to :"+to);
+			 Session session = Session.getDefaultInstance(props, null);
+	    
+			 MimeMessage message = new MimeMessage(session);
+			 message.setFrom(new InternetAddress(from));
+
+			 InternetAddress toAddress = new InternetAddress(to);
+	   // InternetAddress[] toAddress = new InternetAddress[to.length];
+	    // To get the array of addresses
+	   // for( int i=0; i < to.length; i++ ) { // changed from a while loop
+	      //  toAddress = new InternetAddress();
+	    //}
+			 System.out.println(Message.RecipientType.TO);
+	   
+	   // for( int i=0; i < toAddress.length; i++) { // changed from a while loop
+			 message.addRecipient(Message.RecipientType.TO, toAddress);
+			 System.out.println(toAddress);
+	    //}
+			 message.setSubject("Flipkart : Product is in Stock!!! ");
+			 message.setText("Hello!! You will be delighted to know that your product is in stock--Please login to buy :)");
+	    
+			 Transport transport = session.getTransport("smtp");
+			 transport.connect(host, from, pass);
+			 transport.sendMessage(message, message.getAllRecipients());
+			 transport.close();  
+			 System.out.println("Mail sent....");
+	   }
+	 catch(Exception e)
+	 {
+		 System.out.println(e);
+	 }
+		 
+		
+		 
+		}
+		
+		 Stock.deleteNotifyme(sql);	
+		
+	}
+
 	public ArrayList<Product> getList() {
 		return list;
 	}
